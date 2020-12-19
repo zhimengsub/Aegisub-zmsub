723d01d13088d9b1bccc821f1129d045a0b74e2c
wangqr <wangqr@wangqr.tk>
2020-12-19 22:20:15 +0800

    Fix compatibilities with vcpkg

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -9,7 +9,6 @@
 #   FFTW_INCLUDES            ... fftw include directory
 #
 # The following variables will be checked by the function
-#   FFTW_USE_STATIC_LIBS    ... if true, only static libraries are found
 #   FFTW_ROOT               ... if set, the libraries are exclusively searched
 #                               under this path
 #   FFTW_LIBRARY            ... fftw library to use
@@ -27,15 +26,6 @@ find_package(PkgConfig QUIET)
 #Determine from PKG
 if( PKG_CONFIG_FOUND AND NOT FFTW_ROOT )
   pkg_check_modules( PKG_FFTW QUIET "fftw3" )
-endif()
-
-#Check whether to search static or dynamic libs
-set( CMAKE_FIND_LIBRARY_SUFFIXES_SAV ${CMAKE_FIND_LIBRARY_SUFFIXES} )
-
-if( ${FFTW_USE_STATIC_LIBS} )
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
-else()
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
 endif()
 
 if( FFTW_ROOT )
@@ -108,8 +98,6 @@ set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTWF_LIB})
 if(FFTWL_LIB)
   set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
 endif()
-
-set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_SAV} )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW DEFAULT_MSG
